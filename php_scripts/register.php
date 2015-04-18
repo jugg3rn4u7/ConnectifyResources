bab0f52e7c5eeb14914d744b2839828da40568de
shrikant <shriku4u27@gmail.com>
2015-04-17 23:14:52 -0500

    UUID Bug Fix

--- php_scripts/register.php
+++ php_scripts/register.php
@@ -1,5 +1,3 @@
-<html>
-
 <?php
 // PHP variable to store the host address
 $dbhost = 'connectifydbinstance.cl7ia31oqlit.us-east-1.rds.amazonaws.com';
@@ -19,23 +17,23 @@ $dbname = 'connectifydb';
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 
-	 $userid = $_POST["id"];
      $firstname = $_POST["firstname"];
      $lastname = $_POST["lastname"];
      $password = $_POST["password"];
-	 $emailid= $_POST["emailid"];
-	 
-	 echo $firstname."<br>";
-	 echo $lastname."<br>";
-	 echo $password."<br>";
-	 echo $emailid."<br>";
+	   $emailid= $_POST["emailid"];
    
-   $query = "INSERT INTO user VALUES ('$userid','$firstname','$lastname','$emailid','$password');";
+   $query = "INSERT INTO user(user_FirstName, user_LastName, user_email, password ) VALUES ('$firstname','$lastname','$emailid','$password');";
+
    $result = mysqli_query($link,$query);
   
    $count = mysqli_affected_rows($link);
-   echo $count;
-   echo $result;
+
+   $query = "select user_id FROM user where user_email = '$emailid'";
+   $result = mysqli_query($link,$query);
+   while ($row = mysqli_fetch_row($result)) 
+   {
+      $output["user_id"] = $row[0];
+   }
    
     if($count > 0)
      {
@@ -48,5 +46,3 @@ $dbname = 'connectifydb';
 
      print(json_encode($output));
 ?>
-
-</html>
